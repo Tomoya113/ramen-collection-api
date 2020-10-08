f3525ce1ba515e6cef0fd8fb22078c0ae813ecae
Tomoya Tanaka <tomoya113.code@gmail.com>
2020-10-09 02:58:30 +0900

    hoge

--- app/controllers/api/v1/station_users_controller.rb
+++ app/controllers/api/v1/station_users_controller.rb
@@ -12,7 +12,7 @@ module Api
         
         station_user = StationUser.where(station_id: params[:station_id]).where(user_id: params[:user_id])
         
-        shop_stations = ShopStation.where(station_id: station_user.station_id)
+        shop_stations = ShopStation.where(station_id: station_user[0].station_id)
         shop_stations.each do |shop_station| 
           hoge = ShopUser.create!({
             shop_id: shop_station.shop.id,
